7ef8ed5015f6c43ac809445fad6586655051777f
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2020-06-04 19:02:48 -0500

    test: Dictionary examples

--- Program.cs
+++ Program.cs
@@ -18,11 +18,28 @@ namespace school
             Printer.WriteTitle("Welcome to the School");
             //Printer.Beep(10000,500, 10);
             printSchoolCourses(engine.School);
-
             var objectList = engine.GetSchoolObjects();
-
            // engine.School.CleanPlace();
 
+            Dictionary <int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(10, "Alfredo");
+            dictionary.Add(23, "Blue");
+
+            foreach (var key in dictionary)
+            {
+                    WriteLine($"key: {key.Key} Value: {key.Value}");
+            }
+
+            Printer.WriteTitle("Dictionary access");
+            dictionary[0] = "Moss";
+            WriteLine(dictionary[0]);
+
+            Printer.WriteTitle("Another dictionary");
+            var dic = new Dictionary<string, string>();
+            dic["Luna"] = "Cuerpo celeste";
+            WriteLine(dic["Luna"]);
+            dic["Luna"] = "Main charcater";
+            WriteLine(dic["Luna"]);
         }
 
         private static void printSchoolCourses(School school)
